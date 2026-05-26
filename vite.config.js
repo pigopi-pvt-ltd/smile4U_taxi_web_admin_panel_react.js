adccd99b4988488e730883e2b81a28ed45153bfa
Anikrawat <anikrawat18@gmail.com>
2026-05-26 19:28:47 +0530

    Fixed Issues

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@api': path.resolve(__dirname, './src/api'),
-      '@store': path.resolve(__dirname, './src/store'),
-    }
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@store": path.resolve(__dirname, "./src/store"),
+    },
   },
   server: {
     port: 5000,
     open: true,
     proxy: {
-      '/auth': {
-        target: 'http://localhost:3000',
+      "/auth": {
+        target: "http://p69ewng0uhoyo2jaeq93jpjb.82.29.164.173.sslip.io",
         changeOrigin: true,
         secure: false,
       },
-      '/user': {
-        target: 'http://localhost:3000',
+      "/user": {
+        target: "http://p69ewng0uhoyo2jaeq93jpjb.82.29.164.173.sslip.io",
         changeOrigin: true,
         secure: false,
       },
-      '/admin': {
-        target: 'http://localhost:3000',
+      "/admin": {
+        target: "http://p69ewng0uhoyo2jaeq93jpjb.82.29.164.173.sslip.io",
         changeOrigin: true,
         secure: false,
       },
-      '/upload': {
-        target: 'http://localhost:3000',
+      "/upload": {
+        target: "http://p69ewng0uhoyo2jaeq93jpjb.82.29.164.173.sslip.io",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
+
